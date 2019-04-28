f10450f3152419dc2b3ce878b0986a4bc2b48ba3
Francis Nduamaka <francis.nduamaka@gmail.com>
2019-04-28 16:56:54 +0100

    feature(user login): Implement user login endpoint write validation checks for user's input write tests for all validations [Delivers #165604098]

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -39,11 +39,42 @@ class UserController {
       isAdmin,
     };
 
-    users.push(data);
+    const user = {
+      id,
+      email,
+      firstName,
+      lastName,
+      password: Authenticator.hashPassword(password),
+      address,
+      status,
+      isAdmin,
+    };
+
+    users.push(user);
     return res.status(201).send({
       status: 201,
       data,
     });
+  }
+
+  /**
+   * @method userLogin
+   * @description login a user
+   * @param {object} req - The Request Object
+   * @param {object} res - The Response Object
+   * @returns {object} JSON API Response
+   */
+  static userLogin(req, res) {
+    const { email } = req.body;
+    const findEmail = users.find(user => user.email === email);
+    const index = users.findIndex(user => user.email === email);
+    if (findEmail && index !== -1) {
+      res.status(200).send({
+        message: 'Login Successful!',
+        status: 200,
+        data: users[index],
+      });
+    }
   }
 }
 
